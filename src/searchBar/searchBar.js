4a18ba2347d5fc659da5f5658e890e3a69e13aad
Christopher Williams <cw@chriswillia.ms>
2019-05-28 16:16:16 -0500

    Adds static state to the SearchBar component.

--- src/searchBar/searchBar.js
+++ src/searchBar/searchBar.js
@@ -5,7 +5,29 @@ class SearchBar extends React.Component {
     render() {
         return (
             <section className='search_bar'>
-                <p>SearchBar placeholder test</p>
+                <div className='search_bar__term'>
+                    <form>
+                        <label htmlFor='search-term'>Search:</label>
+                        <input type='text' name='search-term' id='search-term' placeholder='Interesting subject' />
+                        <button>Search</button>
+                    </form>
+                </div>
+                <div className='search_bar__filters'>
+                    <label htmlFor='print-type'>Print Type: </label>
+                    <select name='print-type' id='print-type'>
+                        <option value='all'>All</option>
+                        <option value='books'>Books</option>
+                        <option value='magazines'>Magazines</option>
+                    </select>
+                    <label htmlFor='book-type'>Book Type: </label>
+                    <select name='book-type' id='book-type'>
+                        <option value='no-filter'>No filter</option>
+                        <option value='partial'>Parts of text are previewable</option>
+                        <option value='free-ebooks'>Free ebooks</option>
+                        <option value='paid-ebooks'>Paid ebooks</option>
+                        <option value='ebooks'>Any ebooks</option>
+                    </select>
+                </div>  
             </section>
         );
     }
